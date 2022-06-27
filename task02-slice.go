8e567a09968894a7deb4a0901287193edf1a3f08
samatt90 <samatt90@gmail.com>
2022-06-27 15:36:55 +0600

    init: solution task02

--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,10 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
+	res := make([]int64, len(input))
+	for i, v := range input {
+		res[len(res)-1-i] = v
+	}
+	copy(result, res)
 	return
 }
